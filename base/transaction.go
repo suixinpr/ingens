5003779c766e79b1b0ddffd3da2962af10a5701b
suixinpr <suixinpr@yeah.net>
2022-06-09 15:39:51 +0800

    基本实现事务管理器

--- base/transaction.go
+++ base/transaction.go
@@ -3,8 +3,12 @@ package base
 type (
 	// Transaction id
 	TransactionId uint64
+
+	CommitSequenceNumber uint64
 )
 
 const (
 	InvalidTid TransactionId = 0
+
+	InvalidCsn CommitSequenceNumber = 0
 )
